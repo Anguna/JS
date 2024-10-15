9c15256880f7164cee442858774f4ae166648b24
Anguna <nathansantosanguna@gmail.com>
2024-10-15 16:50:09 -0300

    treinamento

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade de seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permetida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
